6b084868651c29c61f01aca252cea97f10bbbf98
wangyifan <wangyifan@baiing.cn>
2020-01-07 10:36:51 +0800

    1.第一次提交

---
+++ src/test/java/xyz/xiatiao/majiangbiji/MajiangbijiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.xiatiao.majiangbiji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajiangbijiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
